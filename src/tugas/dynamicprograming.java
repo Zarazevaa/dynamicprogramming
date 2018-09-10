5aa2103aeb604fb03dacbf024724e00f74e98c44
ZEVA <ZEVA@192.168.0.31>
2018-09-10 18:19:37 +0700

    Initial Commit

---
+++ src/tugas/dynamicprograming.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas;
+
+/**
+ *
+ * @author ZEVA
+ */
+public class dynamicprograming {
+    public static void main(String[] args) {
+        String identitas = "Zara Zeva Az Zurra / XRPL3 / 40";
+        
+        tampilJudul(identitas);
+    }
+    private static void tampilJudul(String identitas)
+    {
+        System.out.println("Identitas : " + identitas);
+        
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+    }
+}
