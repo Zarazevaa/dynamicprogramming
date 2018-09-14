8bab86693723f688836c2d13e50d62ccd401a7f2
ZEVA <ZEVA@192.168.0.31>
2018-09-14 19:20:15 +0700

    penambahan array

--- src/tugas/dynamicprogaming2.java
+++ src/tugas/dynamicprogaming2.java
@@ -15,6 +15,13 @@ public class dynamicprogaming2 {
       identitas = "Zara zeva az zurra / 40 / XRPL3";
         System.out.println("Nama / No.absen / Kelas " + identitas);
         
-   
+        Scanner baca = new Scanner (System.in);
+        System.out.print("berat: ");
+        int n = baca.nextInt();
+        double o = 0, i = 0, j = 0, g = 0, m = 0, l, b = 0;
+        double[] y = new double [] {4, 5, 2, 7, 3};
+        
+        
+        
     }
 }
