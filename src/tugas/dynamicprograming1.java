4fadf499db98770c5b2b4bbf78f4ae6e7391c57e
ZEVA <ZEVA@192.168.0.31>
2018-09-14 18:58:09 +0700

    identitas

---
+++ src/tugas/dynamicprograming1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */  
+package tugas;         
+import java.util.Scanner;
+/**
+ *
+ * @author ZEVA 
+ */
+public class dynamicprograming1 {
+    public static void main(String[] args) {
+      String identitas;
+      identitas = "Zara zeva az zurra / 40 / XRPL3";
+        System.out.print("Nama / No.absen / Kelas " + identitas);
+     
+    }
+}
