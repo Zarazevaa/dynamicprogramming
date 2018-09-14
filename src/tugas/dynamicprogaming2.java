a0cd46bbb4156e7db37f553fb670c8c568083336
ZEVA <ZEVA@192.168.0.31>
2018-09-14 19:57:30 +0700

    proses

--- src/tugas/dynamicprogaming2.java
+++ src/tugas/dynamicprogaming2.java
@@ -15,13 +15,44 @@ public class dynamicprogaming2 {
       identitas = "Zara zeva az zurra / 40 / XRPL3";
         System.out.println("Nama / No.absen / Kelas " + identitas);
         
-        Scanner baca = new Scanner (System.in);
-        System.out.print("berat: ");
+     Scanner baca = new Scanner (System.in);
+        System.out.println("Berat : " );
         int n = baca.nextInt();
         double o = 0, i = 0, j = 0, g = 0, m = 0, l, b = 0;
-        double[] y = new double [] {4, 5, 2, 7, 3};
+        int[] Berat = new int [] {4, 5, 2, 7, 3};
+        int[] Harga = new int[] {10000, 7000, 5000, 3000, 2000};
         
+        int berat = Input();
         
+        Proses(berat, Berat, Harga);
         
+    }
+    
+    private static int Input()
+    {
+         Scanner baca = new Scanner (System.in);
+         
+         System.out.print("Berat : ");
+         int berat = baca.nextInt();
+         
+         return berat;
+    }
+    
+    private static void Proses (int berat, int[] Berat, int[] Harga)
+    {
+        int jumlah = 0;
+        for (int i = 0; i < Berat.length; i++) {
+            int n = 0;
+            while (berat >= Berat[i])
+            {
+                berat = berat - Berat[i];
+                n++;
+            }
+            int total = n * Harga[i];
+            System.out.println(Berat[i] + "Sebanyak " + n + " dengan harga Rp" + total);
+            jumlah = jumlah + total;
+            
+        }
+        System.out.println("\nTotal Bayaran: Rp " + jumlah);
     }
 }
